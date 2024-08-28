e57dd9641fb740e62ae3eaab4adaac56af91e85f
Victoria <victoriamariateran@gmail.com>
2024-08-28 20:22:18 -0300

    changes on navbar

--- RepoFrontend/payout/src/components/Navbar.jsx
+++ RepoFrontend/payout/src/components/Navbar.jsx
@@ -7,11 +7,25 @@ import line from '../assets/line.png'
 
 const Navbar = () => {
   return (
-    <div className="w-full h-[10vh] flex-col justify-center">
-      <div className='w-full h-[9.5vh] flex items-center'>
+    <div className="w-full h-[10vh] flex-col justify-center ">
+      <div className='w-full h-[9.5vh] flex  items-center justify-between'>
+      <div className='flex items-center  gap-6 '>
+        <div className='flex items-center'>
       <img className="h-[30px] ml-10 hidden md:block" src={icon} alt=""/>
       <img className="h-[18px] ml-2 hidden md:block" src={payout} alt=""/>
       </div>
+        <a >Personal</a>
+        <a>Empresas</a>
+      </div>
+      <div className='flex gap-6 mx-8 '> 
+        <a>Funciones</a>
+        <a>Precios</a>
+        <a>Ayuda</a>
+        <a>Iniciar sesion</a>
+        <a>crear cuenta</a>
+      </div>
+      </div>
+    
       <img className="w-full " src={line} alt=""/>
       
 
