9b605ae2929cc531200c91cc9171aa3b4d38ecb6
Victoria <victoriamariateran@gmail.com>
2024-09-03 19:36:32 -0300

    correct routes names and footer position

--- RepoFrontend/payout/src/components/footer/Footer.jsx
+++ RepoFrontend/payout/src/components/footer/Footer.jsx
@@ -10,7 +10,7 @@ import  payout  from '../../assets/PAYOUT.png'
 export const Footer = () => { 
 
   return (
-    <div className="md:py-4 bg-secundario flex px-32 gap-10 justify-between items-center fixed bottom-0 w-full">
+    <div className="md:py-4 bg-secundario flex px-32 gap-10 justify-between items-center relative bottom-0 w-full">
         <div className='flex gap-4 items-end'>
             <img src={icon} className='w-[33px] h-[27px]' alt="" />
             <img src={payout} className='w-[136px] h-[18px]' alt="" />
