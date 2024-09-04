abd78da7ffa5ea258154ea274db48e54ff270bd3
Emmanuel-VanDick9 <emmaaavd@gmail.com>
2024-09-04 16:41:23 -0300

    Create file ThemeContext

---
+++ RepoFrontend/payout/src/contexts/theme/ThemeContext.jsx
@@ -0,0 +1,22 @@
+import React, { createContext, useState, useContext } from 'react'
+
+
+const ThemeContext = createContext();
+
+export const useTheme = () => useContext(ThemeContext);
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className={theme}>
+        {children}
+      </div>
+    </ThemeContext.Provider>
+  );
+};
