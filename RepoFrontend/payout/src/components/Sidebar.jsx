27a983f68fcd1d328f1f77a62bd632f8b8b9aa2f
Emmanuel-VanDick9 <emmaaavd@gmail.com>
2024-09-06 23:44:41 -0300

    Create reusable component button to sidebar

--- RepoFrontend/payout/src/components/Sidebar.jsx
+++ RepoFrontend/payout/src/components/Sidebar.jsx
@@ -1,18 +1,35 @@
+import { useState } from 'react';
 import React from 'react';
-import payout from '../assets/iconpayout.png'; 
+import payout from '../assets/iconpayout.png';
+import AsideButton, {options} from './atoms/AsideButton';
 
 const Sidebar = () => {
+
+  // Inicializamos el estado con el ID del primer botón (Inicio)
+  const [selectedId, setSelectedId] = useState(options[0].id);
+
+  // Función para manejar la selección de un botón
+  const handleButtonClick = (id) => {
+    setSelectedId(id);
+  };
+
+
+  /* Desestructuramos name e image de options para aplicar a los botones del sidebar */
+  
   return (
     <div className='md:h-full md:w-full'>
       {/* Sidebar en pantalla completa (solo para desktop) */}
       <div className="hidden md:flex md:flex-col  h-full bg-white text-black p-6">
         <img src={payout} alt="logo" />
         <div className="h-full flex flex-col justify-center gap-10">
-          <button>Inicio</button>
-          <button>Cuenta</button>
-          <button>Perfil</button>
-          <button>Ayuda</button>
-          <button>Cerrar sesión</button>
+        {options.map(({ id, image, name }) => (
+            <AsideButton 
+              key={id} 
+              image={image} 
+              text={name}
+              onClick={() => handleButtonClick(id)}
+              isSelected={selectedId === id} />
+          ))}
         </div>
       </div>
 
