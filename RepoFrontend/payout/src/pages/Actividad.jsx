f97bf73e065996eb0c9c729bb2f7afb3cd615471
Victoria <victoriamariateran@gmail.com>
2024-09-11 11:57:45 -0300

    g

--- RepoFrontend/payout/src/pages/Actividad.jsx
+++ RepoFrontend/payout/src/pages/Actividad.jsx
@@ -5,58 +5,58 @@ import ActivityButton from '../components/atoms/ActivityButton.jsx';
 
 const Actividad = () => {
 
-// Inicializamos el estado con el ID del primer botón (Inicio)
-const [selectedId, setSelectedId] = useState(0);
-
-// Función para manejar la selección de un botón
-const handleButtonClick = (id) => {
-  setSelectedId(id);
-};
-
-    const options = [
-        {
-            id: 1, 
-            name: 'Pagoss'
-        },
-        {
-            id: 2,
-            name: 'Transferencias'
-        },
-        {
-            id: 3,
-            name: 'Ingresos de dinero'
-        },
-    ]
-
-    return (
-        <section className='px-4 md:px-10 w-full '>
-            <h2 className='font-semibold  py-4 text-left'>Mi Actividad</h2>
-            <section className='flex gap-4'>
-
-            {options.map(({ id, name }) => (
-            <ActivityButton 
-              key={id}  
-              text={name}
-              onClick={() => handleButtonClick(id)}
-              isSelected={selectedId === id} />
-          ))}
-
+    // Inicializamos el estado con el ID del primer botón (Inicio)
+    const [selectedId, setSelectedId] = useState(0);
+    
+    // Función para manejar la selección de un botón
+    const handleButtonClick = (id) => {
+      setSelectedId(id);
+    };
+    
+        const options = [
+            {
+                id: 1, 
+                name: 'Pagoss'
+            },
+            {
+                id: 2,
+                name: 'Transferencias'
+            },
+            {
+                id: 3,
+                name: 'Ingresos de dinero'
+            },
+        ]
+    
+        return (
+            <section className='px-4 md:px-10 w-full '>
+                <h2 className='font-semibold  py-4 text-left'>Mi Actividad</h2>
+                <section className='flex gap-4'>
+    
+                {options.map(({ id, name }) => (
+                <ActivityButton 
+                  key={id}  
+                  text={name}
+                  onClick={() => handleButtonClick(id)}
+                  isSelected={selectedId === id} />
+              ))}
+    
+                </section>
+    
+                {/* Recorrer y renderizar cada actividad */}
+                
+                {activities.map(({ id, datetime, actNotification, amount, activityType }) => (
+                    <ActivityItem
+                        key={id}
+                        datetime={datetime}
+                        actNotification={actNotification}
+                        amount={amount}
+                        activityType={activityType}
+                    />
+                ))}
+               
             </section>
-
-            {/* Recorrer y renderizar cada actividad */}
-            
-            {activities.map(({ id, datetime, actNotification, amount, activityType }) => (
-                <ActivityItem
-                    key={id}
-                    datetime={datetime}
-                    actNotification={actNotification}
-                    amount={amount}
-                    activityType={activityType}
-                />
-            ))}
-           
-        </section>
-    );
-}
-
-export default Actividad;
+        );
+    }
+    
+    export default Actividad;
