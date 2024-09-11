924d6917dfeff7b9b99bdc460c49366cab604b5f
Victoria <victoriamariateran@gmail.com>
2024-09-11 12:35:15 -0300

    banner

--- RepoFrontend/payout/src/components/atoms/ActivityButton.jsx
+++ RepoFrontend/payout/src/components/atoms/ActivityButton.jsx
@@ -6,7 +6,7 @@ const ActivityButton = ({  text, isSelected, onClick }) => {
       <button onClick={onClick} // Agregamos el evento onClick 
       className={`flex items-center gap-2 px-2 py-1 w-full text-left flex justify-start font-extrabold  hover:rounded-3xl
         ${isSelected
-          ? "bg-verde text-white font-semibold rounded-3xl shadow-md focus:outline-none "
+          ? "bg-primario text-white font-semibold rounded-3xl shadow-md focus:outline-none "
           : "bg-white  text-black font-semibold rounded-3xl shadow-md hover:bg-grisclaro focus:outline-none focus:ring-5 focus:ring-gris focus:ring-opacity-50" // Sin diseño para no seleccionado
       }`}>
         
