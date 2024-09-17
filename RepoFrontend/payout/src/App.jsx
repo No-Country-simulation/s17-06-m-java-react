999b019f92c54933a430f49bc56c544db6a94c70
Victoria <victoriamariateran@gmail.com>
2024-09-16 23:43:26 -0300

    simplify routes

--- RepoFrontend/payout/src/App.jsx
+++ RepoFrontend/payout/src/App.jsx
@@ -37,6 +37,17 @@ function App() {
     // Aquí puedes poner la lógica que quieres ejecutar cuando se desactiva
   };
 
+//   <div className={`flex flex-col-reverse justify-between lg:flex-row ${light ? 'bg-dark text-white' : 'bg-secundario text-black'} flex-grow`}>
+//   {showSidebar && <Sidebar />}
+//     <div className={`text-end  ${showSidebar ? 'ml-0 w-full md:w-5/6' : 'w-full'} ${light ? 'bg-dark text-white' : 'bg-white text-black'}`}>
+//     <div className="hidden md:block">
+//       <ThemeButton  onActivate={handleActivate} onDeactivate={handleDeactivate} />
+//       </div>
+
+//       <Outlet  />
+
+//     </div>
+// </div>
 
 
 
@@ -44,8 +55,9 @@ function App() {
     <>
       <div className={`app flex flex-col h-screen justify-between ${light ? 'bg-dark text-white' : 'bg-bg-white text-black'}`}>
         {showNavbar && <Navbar light={light} onActivate={handleActivate} onDeactivate={handleDeactivate}/>}
+        <div className={` ${light ? 'bg-dark text-white' : 'bg-secundario text-black'} flex-grow`}>
 
-        <div className={` text-end${light ? 'bg-dark text-white' : 'bg-secundario text-black'} flex-grow`}>
+        <div className={`text-end h-full  ${light ? 'bg-dark text-white' : 'bg-white text-black'}`}>
           
             
             <div className="hidden md:block">
@@ -56,7 +68,7 @@ function App() {
 
             
         </div>
-
+        </div>
         {showFooter && <Footer />}
 
         
