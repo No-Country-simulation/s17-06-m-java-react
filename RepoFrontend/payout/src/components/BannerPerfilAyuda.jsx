2f2160b54753b8bc506d446c67b0a3831fc2fb8b
Clemencia Ibanez Padilla <clemenciaibanezpadilla@gmail.com>
2024-09-17 21:13:19 -0300

    help

--- RepoFrontend/payout/src/components/BannerPerfilAyuda.jsx
+++ RepoFrontend/payout/src/components/BannerPerfilAyuda.jsx
@@ -1,60 +1,29 @@
 import  notif  from '../assets/Notificaciones.png'
-import  userimage  from '../assets/userimage.png'
-import { useEffect, useState } from 'react';
-import { ThemeButton } from './ThemeButton';
 import { Ask } from './icons/ask';
+import { useNavigate } from "react-router-dom";
+import { FlechaTriangulitoBack } from './icons/flechaTriangulitoBack';
 
 
 
-const urlInfoUserLogged = 'https://payout.redromsolutions.com/user/0'
 
 
-
-const Banner = () => {
-
-    const [name, setName] = useState('');
-    const [lastName, setLastName] = useState('');
-
-
- 
-
-
-    /* FETCH PARA TRAER LA INFO DE BASE DE DATOS Y LLENAR LOS CAMPOS */
-    useEffect(() => {
-        fetch(urlInfoUserLogged, {
-            method: 'GET',
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${localStorage.getItem('token')}`
-            }
-        })
-            .then(data => data.json())
-            .then(data => {
-                const userData = data.data[0];
-                setName(userData.userDetail.name);
-                setLastName(userData.userDetail.lastName)
-            });
-    }, []);
-
-
+export const BannerPerfilAyuda = () => {
+    
+    const navigate = useNavigate()   
 
   return (
     <div id='div-total-banner' className='md:hidden flex justify-between items-center w-full'>
         <section className='flex'>
-            <button onClick={handleVolverAtras} className='flex text-white whitespace-nowrap font-semibold'>
+            <button onClick={()=>navigate(-1)} className='flex text-white whitespace-nowrap font-semibold'>
                 <p className='w-6 h-6 absolute left-2 flex items-center'>
                     <span className='left-7 absolute'>Transferir</span>
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M15 18L9 12L15 6" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
+                    <FlechaTriangulitoBack/>
                 </p>
             </button>
             <div className='absolute right-4 flex gap-4'>
                 <img src={notif} className='w-[24px] h-[24px] items-center' alt="" />
                 <p className='text-white '>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
-                    </svg>
+                    <Ask/>
                 </p>
             </div>
         </section>
@@ -62,4 +31,3 @@ const Banner = () => {
   )
 }
 
-export default Banner
---
+++ RepoFrontend/payout/src/components/ayuda/FAQ.jsx
@@ -0,0 +1,78 @@
+import { useNavigate } from "react-router-dom";
+
+import { FlechaTriangulitoBack } from '../icons/flechaTriangulitoBack';
+import Ask from '../icons/ask';
+
+import  notif  from '../../assets/Notificaciones.png'
+import  Flechagrande  from '../../assets/Flechagrande.svg'
+import  Ayuda1  from '../../assets/Ayuda1.svg'
+import  Ayuda2  from '../../assets/Ayuda2.svg'
+
+
+
+
+export const FAQ = () => {
+
+    const navigate = useNavigate()
+
+
+    return (
+        <>
+        {/* BANNER MOBILE*/}        
+        <div id='div-total-banner' className='md:hidden flex justify-between items-center w-full'>
+            <section className='flex'>
+                <button onClick={()=>navigate(-1)} className='flex text-white whitespace-nowrap font-semibold'>
+                    <p className='w-6 h-6 absolute left-2 flex items-center'>
+                        <span className='left-7 absolute'>Preguntas frecuentes</span>
+                        <FlechaTriangulitoBack/>
+                    </p>
+                </button>
+                <div className='absolute right-4 flex gap-4'>
+                    <img src={notif} className='w-[24px] h-[24px] items-center' alt="" />
+                    <p className='text-white '>
+                        <Ask/>
+                    </p>
+                </div>
+            </section>
+        </div>
+
+        <section className='w-full flex flex-col items-center justify-center h-screen px-6 md:px-10'>        
+
+            {/* CUERPO HOME */}
+            <div id='cuerpo-vista-home' className='w-full'>                
+
+
+                {/* TARJETA */}
+                <div id='tarjeta-perfil' className='px-5 py-6 w-full bg-grisclaro text-primario flex flex-col gap-4 mb-5'>  
+                    <div className="border-b border-primario flex flex-col gap-3 pb-3">
+                        <p className='text-start font-bold'>Información general de Payout</p>
+                        <p className='text-start text-verde text-sm'>¿Qué es Payout? ¿Qué podes hacer en la app? Una introducción.</p>                        
+                    </div> 
+                    <div className="border-b border-primario flex flex-col gap-3 pb-3">
+                        <p className='text-start font-bold'>Creación de tu cuenta personal</p>
+                        <p className='text-start text-verde text-sm'>Una guía para  registrarte, verificar tu identidad y configurar tu cuenta.</p>                        
+                    </div>  
+                    <div className="border-b border-primario flex flex-col gap-3 pb-3">
+                        <p className='text-start font-bold'>Depositar Dinero</p>
+                        <p className='text-start text-verde text-sm'>Como ingresar dólares, euros, pesos, reales y mucho mas.</p>                        
+                    </div>    
+                    <div className="border-b border-primario flex flex-col gap-3 pb-3">
+                        <p className='text-start font-bold'>Intercambios</p>
+                        <p className='text-start text-verde text-sm'>Todo lo que necesitas saber para convertir tu saldo en distintas monedas de manera ágil y segura.</p>                        
+                    </div>
+                    <div className="border-b border-primario flex flex-col gap-3 pb-3">
+                        <p className='text-start font-bold'>Enviar dinero</p>
+                        <p className='text-start text-verde text-sm'>Como transferir fondos a otras cuentas, tanto locales como internacionales.</p>                        
+                    </div>
+                    <div className="flex flex-col gap-3 pb-3">
+                        <p className='text-start font-bold'>Payout Mastercard ©️</p>
+                        <p className='text-start text-verde text-sm'>Una tarjeta prepaga con validez internacional para usar tus pesos o monedas donde quieras.</p>                        
+                    </div>            
+                </div>                 
+
+            </div>            
+        </section>
+        </>
+        
+    );
+}
